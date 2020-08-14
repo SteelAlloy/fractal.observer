3cc9f1be12cf3027d1bcf6d9c70a6aec71ee77bf
oganexon <oganexon@protonmail.com>
2020-08-14 15:13:41 +0200

    feat: interactive map

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 /* global createREGL, mat2, vec2 */
 
-if ('serviceWorker' in navigator) {
+/* if ('serviceWorker' in navigator) {
   window.addEventListener('load', function () {
     navigator.serviceWorker
       .register('/serviceWorker.js')
       .then(res => console.log('service worker registered'))
       .catch(err => console.log('service worker not registered', err))
   })
-}
+} */
+
+let x, y
+
+document.addEventListener('mousemove', (event) => {
+  x = event.clientX / window.innerWidth
+  y = event.clientY / window.innerHeight
+  // console.log(x, y)
+})
 
 const regl = createREGL()
 const draw = regl({
@@ -17,9 +25,11 @@ const draw = regl({
     #else
       precision mediump float;
     #endif
+
     
-    float mandelbrot(vec2 z, vec2 c) {
+    float mandelbrot(vec2 z, vec2 c, float maxIteration) {
       for(float i = 0.0; i < 1e3; i++) {
+        if (i == maxIteration) break;
         float xx = z.x*z.x, yy = z.y*z.y, zz = xx + yy;
         if(zz > 16.0) return i - log2(log2(zz)) + 4.0;
         z = vec2(xx - yy, z.x*z.y*2.0) + c;
@@ -27,9 +37,10 @@ const draw = regl({
     }
     
     uniform vec2 c;
+    uniform float maxIteration;
     varying vec2 z;
     void main() {
-      float m = mandelbrot(z, c);
+      float m = mandelbrot(z, c, maxIteration);
       vec3 rgb = 0.5 + 0.5*cos(3.0 + 0.15*m + vec3(0.0, 0.6, 1.0));
       gl_FragColor = vec4(rgb, 1.0);
     }
@@ -48,10 +59,13 @@ const draw = regl({
       return mat2.fromScaling([], [w / h, 1])
     },
     c: ({ time: t }) => {
-      const a = [-0.765, 0.153]
-      const b = [Math.sin(t / 5), Math.cos(t / 5)]
+      // const a = [-0.765, 0.153]
+      const a = [(x - 0.5) * 2, (y - 0.5) * 2]
+      // const b = [Math.sin(t / 5), Math.cos(t / 5)]
+      const b = [0, 0]
       return vec2.scaleAndAdd([], a, b, 2e-2)
-    }
+    },
+    maxIteration: 1e3
   },
   attributes: {
     position: [1, 1, -1, 1, 1, -1, -1, -1]
